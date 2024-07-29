7aa1f039a5870368b66cd45b10600a5b5023fc94
kadir._.ertas <kdrerts287@gmail.com>
2024-07-29 23:58:08 +0300

    bu projeler eğitim amaçlı yapılan projelerdir(bu yükleme klasör proje birleştirmek için yapıldı proje oluşturma 05/2022)

---
+++ Learning_Projects/GenerictsIntro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerictsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            MyList<string> myList= new MyList<string>();
+
+
+            myList.Add("ahmet");
+            Console.WriteLine(myList.Length);
+            myList.Add("mehmet");
+            Console.WriteLine(myList.Length);
+            
+
+        }
+    }
+}
